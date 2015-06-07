6ab50e286688ac5b6311bb8ee55e488c2facd2ff
Johann Diedrick <jdiedrick@gmail.com>
2015-06-06 21:10:51 -0400

    closes #1

--- debbie.py
+++ debbie.py
@@ -1,24 +1,74 @@
-import requests
+import grequests
 from debbieMailer import sendMail
 from debbie_globals import debbie_recipient
+import ssl_fix
+import sqlite3
 
-def isOnView(object):
+objectIDs = [547802, 483879, 45417, 76970, 265169, 480895, 292043, 45434, 467642, 500666]
+
+def scrapiURLFromOID(objectID):
+	scrapiURL = "http://scrapi.org/object/" + str(objectID)
+	return scrapiURL
+
+def isOnView(object, *args, **kwargs):
 	objectNumber = object.json()['CRDID']
 	objectOnViewStatus = object.json()['galleryLink']
+	print objectOnViewStatus
 	if objectOnViewStatus == 'This artwork is not on display':
 		print 'not on view'
-		return False
+		return None
 	else:
 		print 'on view'
 		recipient = debbie_recipient
-		sendMail(recipient, objectNumber)	
-		return True
+		sendMail(recipient, objectNumber, True)	
+		return None
 
-artworkOnView = requests.get('http://scrapi.org/object/547802?fields=galleryLink') #temple of dendur
+def hook_factory(*factory_args, **factory_kwargs):
+	def response_hook(object, *request_args, **request_kwargs):
+		objectNumber =object.json()['CRDID']
+		objectOnViewStatus = object.json()['galleryLink']
+		#print objectOnViewStatus
+		if objectOnViewStatus == 'This artwork is not on display':
+		#print 'not on view'
+			return None
+		else:
+		#print 'on view'
+			new_username = factory_kwargs['new_recipient']
+			print "after request " + "u: " + new_username + " obj: " + str(objectNumber)
+			sendMail(new_username, objectNumber, False)	
+			return None
+		return None
+	return response_hook
 
-artworkNotOnView = requests.get('http://scrapi.org/object/483879?fields=galleryLink') #cy twombly
+#db code
+db = sqlite3.connect('db/debbie.sqlite')
+cursor = db.cursor()
+cursor.execute('SELECT username, objectID, is_on_view, has_sent FROM objects')
 
+#turn our objects into something managable
+objects = []
 
-isOnView(artworkOnView)
+#requests
+rs = []
 
-isOnView(artworkNotOnView)
+for object in cursor.fetchall():
+	username = object[0]
+	objectid = object[1]
+	url = scrapiURLFromOID(object[1])
+	is_on_view = object[2]
+	has_sent = object[3]
+	newObject = {
+			'username': username, 
+			'url': url, 
+			'is_on_view': is_on_view, 
+			'has_sent': has_sent, 
+			'object_id': objectid
+			}
+	objects.append(newObject) 
+	new_username=newObject['username']
+	url = newObject['url']
+	objid = newObject['object_id']
+	print "before request: " + "u: " + new_username + " and " + " obj: " + str(objid)
+	rs.append( grequests.get(url, hooks={'response': [hook_factory(new_recipient=new_username)]}) ) 
+
+grequests.map(rs)
--- debbieMailer.py
+++ debbieMailer.py
@@ -1,14 +1,25 @@
 import smtplib
 from debbie_globals import debbie_email, debbie_password
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
 
 
-def sendMail(recipient, objectNumber):
-	sender = debbie_email 
-	password = debbie_password
-	msg = "Your object is on view! \nFind out where at http://www.metmuseum.org/collection/the-collection-online/search/" + str(objectNumber)
-	server = smtplib.SMTP('smtp.gmail.com', 587)
-	server.starttls()
-	server.login(sender, password)
-	print msg
-	server.sendmail(sender, recipient, msg)
-	server.quit()
+def sendMail(recipient, objectNumber, debug):
+	if debug:
+		print "debug mode. will send object number " + str(objectNumber) + " to recipient: " + recipient 
+	else:
+		sender = debbie_email 
+		password = debbie_password
+		msg = MIMEMultipart()
+		msg['From'] = sender
+		msg['To'] = recipient
+		msg['Subject'] = "Your object is on view at the Met!"
+		body = "Your object is on view! \nFind out where at http://www.metmuseum.org/collection/the-collection-online/search/" + str(objectNumber)
+		msg.attach(MIMEText(body, 'plain'))
+		server = smtplib.SMTP('smtp.gmail.com', 587)
+		server.starttls()
+		server.login(sender, password)
+		text = msg.as_string()
+		server.sendmail(sender, recipient, text)
+		print "sending mail to " + recipient
+		server.quit()
